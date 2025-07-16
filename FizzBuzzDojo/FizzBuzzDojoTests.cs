f0c8a670ffcdeb3634d78e30af41c731680eb5e1
boren.fu <boren.fu@nextfortune.tw>
2025-07-16 16:06:54 +0400

    Refactor FizzBuzzDojoTests to use a single instance of CalculateService and add FizzBuzz fail test case

--- FizzBuzzDojo/FizzBuzzDojoTests.cs
+++ FizzBuzzDojo/FizzBuzzDojoTests.cs
@@ -4,9 +4,12 @@ namespace FizzBuzzDojo;
 
 public class FizzBuzzDojoTests
 {
+    private CalculateService _calculate;
+
     [SetUp]
-    public void Setup()
+    public void SetUp()
     {
+        _calculate = new CalculateService();
     }
 
     [Test]
@@ -14,19 +17,17 @@ public class FizzBuzzDojoTests
     [TestCase(2, "1,2")]
     public void normal_case_when_number_is_not_altered(int maxNumber, string expected)
     {
-        var calculate = new CalculateService();
-        var result = calculate.Result(maxNumber);
+        var result = _calculate.Result(maxNumber);
 
         result.Should().Be(expected);
     }
-    
+
     [Test]
     [TestCase(3, "1,2,Fizz")]
     [TestCase(4, "1,2,Fizz,4")]
     public void fizz_case_when_number_is_divided_by_3(int maxNumber, string expected)
     {
-        var calculate = new CalculateService();
-        var result = calculate.Result(maxNumber);
+        var result = _calculate.Result(maxNumber);
 
         result.Should().Be(expected);
     }
@@ -35,8 +36,16 @@ public class FizzBuzzDojoTests
     [TestCase(5, "1,2,Fizz,4,Buzz")]
     public void buzz_case_when_number_is_divided_by_5(int maxNumber, string expected)
     {
-        var calculate = new CalculateService();
-        var result = calculate.Result(maxNumber);
+        var result = _calculate.Result(maxNumber);
+
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    [TestCase(15, "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz")]
+    public void fizzbuzz_case_when_number_is_divided_by_3_and_5(int maxNumber, string expected)
+    {
+        var result = _calculate.Result(maxNumber);
 
         result.Should().Be(expected);
     }
