56c4a888422e33106b608573e595d02fab637c48
Jonas.Huynh <jonas.huynh@starixsoft.com>
2025-07-17 16:20:33 +0400

    Add initial BankAccountKata project and basic deposit test case

---
+++ BankAccountKata/BankAccountKataTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace BankAccountKata;
+
+public class BankAccountKataTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void deposit_case_when_account_is_created_balance_0()
+    {
+        var bankAccountModel = new BankAccountModel();
+        bankAccountModel.StartAccount();
+        bankAccountModel.balance.Should().Be(0);
+    }
+}
+
+public class BankAccountModel
+{
+    public decimal balance { get; set; }
+
+    public void StartAccount()
+    {
+        balance = 0;
+    }
+}
