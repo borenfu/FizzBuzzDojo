6b066f373cbd16d996275506b2162ae0cba39198
Jonas.Huynh <jonas.huynh@starixsoft.com>
2025-07-24 16:51:14 +0400

    add missing file

---
+++ BankAccountKata/BankAccountModel.cs
@@ -0,0 +1,37 @@
+﻿namespace BankAccountKata;
+
+public class BankAccountModel
+{
+    private decimal Balance { get; set; }
+
+    public void CreateAccount()
+    {
+        Balance = 0;
+    }
+
+    public decimal GetBalance()
+    {
+        return Balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be positive.");
+        }
+        
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+        {
+            throw new InvalidOperationException("Not enough balance to withdraw.");
+        }
+        
+        Balance -= amount;
+        
+    }
+}
