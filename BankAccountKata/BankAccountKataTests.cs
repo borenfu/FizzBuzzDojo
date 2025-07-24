ae6f7f87aa2742544c240f6b246ea6dd3033da4e
Jonas.Huynh <jonas.huynh@starixsoft.com>
2025-07-24 16:22:48 +0400

    Refactor BankAccountKataTests to use a shared instance of BankAccountModel in tests

--- BankAccountKata/BankAccountKataTests.cs
+++ BankAccountKata/BankAccountKataTests.cs
@@ -4,25 +4,25 @@ namespace BankAccountKata;
 
 public class BankAccountKataTests
 {
+    private BankAccountModel _bankAccountModel;
+
     [SetUp]
     public void Setup()
     {
+        _bankAccountModel = new BankAccountModel();
+        _bankAccountModel.CreateAccount();
     }
 
     [Test]
     public void create_account_and_balance_is_0()
     {
-        var bankAccountModel = new BankAccountModel();
-        bankAccountModel.CreateAccount();
-        bankAccountModel.GetBalance().Should().Be(0);
+        _bankAccountModel.GetBalance().Should().Be(0);
     }
-    
+
     [Test]
     public void deposit_100_balance_100()
     {
-        var bankAccountModel = new BankAccountModel();
-        bankAccountModel.CreateAccount();
-        bankAccountModel.Deposit(100);
-        bankAccountModel.GetBalance().Should().Be(100);
+        _bankAccountModel.Deposit(100);
+        _bankAccountModel.GetBalance().Should().Be(100);
     }
 }
